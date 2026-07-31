e09fb5cd019d5debde37f6b3ac7d13647945df65
litkoit1003 <danakuckanov@gmail.com>
2026-07-31 10:54:33 +0300

    Initial commit

---
+++ src/client/java/org/litkoit/client/DirectorsCutWorkstationClient.java
@@ -0,0 +1,10 @@
+package org.litkoit.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DirectorsCutWorkstationClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
